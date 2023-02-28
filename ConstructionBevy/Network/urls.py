69609bcde5c7e01294656f41e5816053dfc7b6ac
Lyanna Guo <junyig@andrew.cmu.edu>
2023-02-28 09:40:16 -0500

    uncomment

--- ConstructionBevy/Network/urls.py
+++ ConstructionBevy/Network/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import PostListView, PostDeleteView, CreateThread, ListThreads, ThreadView, CreateMessage, DeleteThread, DeleteMessage
 
 urlpatterns = [
-    # path('', PostListView.as_view(), name='post-list'),
-    # path('post/delete/<int:pk>', PostDeleteView.as_view(), name='post-delete'),
+    path('', PostListView.as_view(), name='post-list'),
+    path('post/delete/<int:pk>', PostDeleteView.as_view(), name='post-delete'),
     path('inbox/', ListThreads.as_view(), name='inbox'),
     path('inbox/create-thread/', CreateThread.as_view(), name='create-thread'),
     path('inbox/<int:pk>', ThreadView.as_view(), name='thread'),
